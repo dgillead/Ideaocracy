c3fee4c304eacdac69762bb9f29c5776917233bd
Darnell Gillead <dlgillead@gmail.com>
2017-08-18 19:54:30 -0500

    integrated React with Rails

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,9 @@
+let Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'site#index'
+
   namespace :api do
     namespace :v1 do
-      resources :ideas, only: [:index, :create, :destroy, :update] 
+      resources :ideas, only: [:index, :create, :destroy, :update]
     end
   end
 end
