e095a7b7bd3d06f30d6d1cce51e3fdae000f2494
Darnell Gillead <dlgillead@gmail.com>
2017-08-19 14:23:28 -0500

    created main landing page

---
+++ app/assets/javascripts/components/_app_nav.js.jsx
@@ -0,0 +1,9 @@
+let AppNav = React.createClass({
+  render() {
+    return (
+    <nav className="navbar navbar-inverse bg-inverse">
+      <a className="navbar-brand" href="#">Ideaocracy</a>
+    </nav>
+    )
+  }
+})
--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,8 +1,14 @@
 let Main = React.createClass({
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
+      <div className="jumbotron">
+        <h1 className="text-center" id="title">Idea<em>Ocracy</em></h1>
+        <hr />
+        <h2 className="text-center" id="tagline">Clarify, collaborate and codify ideas into future projects</h2>
+        <div className="text-center">
+          <button className="btn btn-success intro-buttons">View Ideas</button>
+          <button className="btn btn-info intro-buttons">Create Idea</button>
+        </div>
       </div>
     )
   }
